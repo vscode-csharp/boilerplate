0c351dd2def334d61698c7cbcf7215d5f321a231
fmcarvalho <mcarvalho@cc.isel.ipl.pt>
2017-02-02 20:46:57 +0000

    Rename Primes.Test namespace

--- test/Primes.Tests/PrimesSupplierTests.cs
+++ test/Primes.Tests/PrimesSupplierTests.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Xunit;
 
-namespace Primes.Test
+namespace Primes.Tests
 {
     public class PrimesSupplierTests
     {
@@ -17,8 +17,10 @@ namespace Primes.Test
         [Fact]
         public void FuncTest() 
         {
+            int[] expected1 = {2, 3, 5, 7, 11, 13, 17};
+            Assert.Equal(expected1, PrimesSupplier.Stream().Take(expected1.Length));
             int[] expected = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31};
-            var primes = ToIterator(PrimesSupplier.Func()).Take(expected.Length);
+            var primes = ToIterator(PrimesSupplier.Func()).Take(expected.Length).ToArray();
             Assert.Equal(expected, primes);
         }
 
